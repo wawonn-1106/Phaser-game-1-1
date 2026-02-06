2e37d4239f8ad600b8e59e39579e495130b0904e
wawonn-1106 <aito.n1106@icloud.com>
2026-02-06 09:47:58 +0900

    I add volume slider.

--- src/contents/SettingsContent.js
+++ src/contents/SettingsContent.js
@@ -8,9 +8,52 @@ export default class SettingsContent{
         const bg=this.uiScene.add.image(0,0,'menu-bg').setDisplaySize(1000,600);
         container.add(bg);
 
+        const title=this.uiScene.add.text(0,-250,'設定',{
+            fontSize:'32px',
+                //
+        });
+        container.add(title);
 
+        const bgmSlider=this.createSlider(-200,-100,'BGM音量',(vol)=>{
+            this.uiScene.sound.volume=vol;//まだ作ってない
+            //sound.bgmの名前.volumeにしてね。
+        });
+        container.add(bgmSlider);
 
-        
         return container;
+    }
+    createSlider(x,y,labelText,callback){
+        const sliderGroup=this.uiScene.add.container(x,y);
+
+        const track=this.uiScene.add.image(0,0,'slider-track');//まだ
+        const trackWidth=track.displayWidth;
+
+        const label=this.uiScene.add.text(-trackWidth,-40,labelText,{
+            fontSize:'24px',
+            color:'#000000'
+        });
+
+        const currentVol=this.uiScene.sound.volume;//同様に名前もね
+
+        const initialX=(currentVol*trackWidth)-(trackWidth/2);
+
+        const handle=this.uiScene.add.image(initialX,0,'slider-handle')
+            .setInteractive({draggable:true,useHandCursor:true});
+        
+        handle.on('drag',(pointer,dragX)=>{
+            const minX=-trackWidth/2;
+            const maxX=trackWidth/2;
+
+            handle.x=Phaser.Math.Clamp(dragX,minX,maxX);
+
+            const volume=(handle.x-minX)/trackWidth;
+
+            callback(volume);
+        });
+
+        sliderGroup.add([track,label,handle]);
+
+        return sliderGroup;
+        //return sliderContainer;
     }
 }
